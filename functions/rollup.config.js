61e1d993389e22c218632adca5d29b6c50aa1713
Ishan Chhabra <me@ishanchhabra.com>
2023-07-10 13:31:30 +0530

    ✨ Init origin request function

---
+++ functions/rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+
+const plugins = [
+  typescript({
+    tsconfig: "./tsconfig-build.json",
+  }),
+  resolve(),
+  commonjs(),
+  replace({
+    "process.env.CLOUDINARY_ID": JSON.stringify(process.env.CLOUDINARY_ID),
+    preventAssignment: true,
+  }),
+];
+
+function makeEntryPointFor(input) {
+  return {
+    input,
+    output: {
+      dir: "dist",
+      format: "cjs",
+    },
+    plugins,
+  };
+}
+
+export default [makeEntryPointFor("./lambdas/origin-request.ts")];
