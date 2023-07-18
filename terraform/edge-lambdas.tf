8424b5c0001871b132cfc4d2633411e209d51cec
astha <astha@rigi.club>
2023-07-18 15:04:43 +0530

    🔧 Add s3 role to lambda

--- terraform/edge-lambdas.tf
+++ terraform/edge-lambdas.tf
@@ -20,6 +20,28 @@ resource "aws_iam_role_policy_attachment" "lambda" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_policy" "s3_policy" {
+  name = "s3-full-access-policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
+  policy_arn = aws_iam_policy.s3_policy.arn
+  role       = aws_iam_role.lambda.id
+}
+
 data "aws_iam_policy_document" "assume" {
   statement {
     actions = ["sts:AssumeRole"]
